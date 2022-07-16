889cab9631966e4aadabd8002f2ed1dad40f517e
Clash2453 <spasmilenkov123@gmail.com>
2022-07-16 14:31:38 +0300

    Allow the program to calculate from prompt

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
-// const compute = document.getElementById('compute');
-// const input = prompt('Enter equation').replace(' ', '').split('')
-// compute.addEventListener('click', operate())
-// const equation = document.getElementById('results').textContent.replace(' ', '');
+const compute = document.getElementById('compute');
+const input = prompt().replace(' ', '').split('')
+compute.addEventListener('click', operate())
+const equation = document.getElementById('results').textContent.replace(' ', '');
+
 function parseString(arr){
     while(arr.length !== 1){
+        let multiplication = arr.indexOf('*');
+        let division = arr.indexOf('/');
+        while(multiplication !== -1 || division !== -1){
+            if(multiplication === -1)
+                multiplication = 999;
+            if(division === -1)
+                division = 999;
+            if(multiplication < division){
+                    let before = arr.slice(0, multiplication-1);
+                    let after = arr.slice(multiplication+2);
+                    before.push(operate(arr[multiplication-1], arr[multiplication+1], '*'));
+                    arr = before.concat(after);
+            }
+                if(multiplication > division){
+                    let before = arr.slice(0, division-1);
+                    let after = arr.slice(division+2);
+                    before.push(operate(arr[division-1], arr[division+1], '/'));
+                    arr = before.concat(after);
+            }
+            multiplication = arr.indexOf('*');
+            division = arr.indexOf('/');
+        }
         let product = operate(arr[0], arr[2],arr[1]);
         arr.splice(0, 3);
         arr.unshift(product);
@@ -13,13 +36,13 @@ function parseString(arr){
 function operate(x, y, operator){
     switch(operator){
         case '+':
-            return add(x,y);
+            return add(Number(x),Number(y));
         case '-':
-            return subtract(x,y);
+            return subtract(Number(x),Number(y));
         case '*':
-            return multiply(x,y);
+            return multiply(Number(x),Number(y));
         case '/':
-            return divide(x,y);
+            return divide(Number(x),Number(y));
     }
 }
 
@@ -38,5 +61,5 @@ function multiply(x,y){
 function divide(x,y){
     return multiply(x, 1 / y)
 }
-// parseString(input)
-parseString([3, '+', 1, '+', 5, '-', 2]);
+parseString(input)
+// parseString([5,'+',3,'+',4,'/',2,'*', 5]);
